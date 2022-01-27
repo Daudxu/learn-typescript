3feafa9294baf0cbc6f4708517be2bcac3d6ea5e
xudan <xudan@apowo.com>
2022-01-27 23:00:35 +0800

    fix: 类型注解，类型推断

--- ts_demo1.ts
+++ ts_demo1.ts
@@ -1,3 +1,27 @@
+// type annotation 类型注解，我们来告诉 TS 变量是什么类型
+// type inference 类型推断， TS 会自动的去尝试分析变量类型
+// 如果 TS 能够自动分析变量类型，我们就什么也不需要做了
+// 如果 TS 无法分析变量类型的话，我们就需要使用类型注解
+
+// let count: number
+// count = 123
+
+//类型注解
 const a: number = 1
 const b: string = 'hello'
 console.log(a, b)
+
+//类型推断
+const c = 1
+const d = 2
+console.log(c + d)
+
+function getTotal(a: number, b: number) {
+  return a + b
+}
+console.log(getTotal(1, 2))
+
+const obj = {
+  name: 'dell',
+  age: 18,
+}
