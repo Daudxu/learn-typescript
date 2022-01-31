5b4b344636552c3e2bd3343b1837ac9267f44f30
daud <875126243@qq.com>
2022-01-31 11:07:26 +0800

    fix ts

---
+++ ts_demo8.ts
@@ -0,0 +1,15 @@
+let isDone:boolean = false
+
+let age:number = 10
+
+let firstName:string = `hello ${age}`
+
+let message: string =  `hello`
+
+let u:undefined = undefined
+let n:null = null
+
+let notSure:any = 4
+notSure = 'mybe a string'
+notSure = true
+
