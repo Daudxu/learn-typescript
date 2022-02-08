9855dc49979ccb02ce784ab28320a56f5d318d1d
xudan <xudan@apowo.com>
2022-02-08 23:36:30 +0800

    泛型

--- generics.ts
+++ generics.ts
@@ -17,3 +17,18 @@ const result2 = swap(['string', 123])
 result2[1]
 
 // 约束泛型
+interface sa {
+  length: number
+}
+
+function toc<T extends sa>(arg: T): T {
+  console.log(arg.length)
+  return arg
+}
+
+const str = toc('aaaa')
+const obja = toc({ length: 3 })
+const arr2 = toc([1, 2, 3, 4, 5, 6])
+console.log(str)
+console.log(obja)
+console.log(arr2)
