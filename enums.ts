de31dd5894d0093ca05e7db8fb1de2dfd5f85d1e
xudan <xudan@apowo.com>
2022-02-08 22:38:23 +0800

    枚举 常量值 计算值

--- enums.ts
+++ enums.ts
@@ -1,8 +1,30 @@
-enum Direction {
-  Up,
-  Down,
-  Left,
-  Right,
+// enum Direction {
+//   Up,
+//   Down,
+//   Left,
+//   Right,
+// }
+// console.log(Direction.Up)
+// console.log(Direction[0])
+
+// enum Direction {
+//   Up = 'Up',
+//   Down = 'Down',
+//   Left = 'Left',
+//   Right = 'Right',
+// }
+// const value = 'Up'
+// if (value === Direction.Up) {
+//   console.log('GO UP!')
+// }
+// 常量值 计算值
+const enum Direction {
+  Up = 'Up',
+  Down = 'Down',
+  Left = 'Left',
+  Right = 'Right',
 }
-console.log(Direction.Up)
-console.log(Direction[0])
+const value = 'Up'
+if (value === Direction.Up) {
+  console.log('GO UP!')
+}
